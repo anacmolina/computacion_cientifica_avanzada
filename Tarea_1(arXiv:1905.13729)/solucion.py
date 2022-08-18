0a362534d1f935207c9308be2af51a558b5277ba
anacmolina <anacmolina@udea.edu.co>
2022-08-17 21:35:36 -0500

    Adding sorted and generator l, k functions

--- Tarea_1(arXiv:1905.13729)/solucion.py
+++ Tarea_1(arXiv:1905.13729)/solucion.py
@@ -4,7 +4,11 @@ import matplotlib.pyplot as plt
 n = 30
 
 def linear_combination(x, y):
-    return np.sum(x*(y**2))*x - np.sum((x**2)*y)*y
+    result = np.sum(x*(y**2))*x - np.sum((x**2)*y)*y
+    return np.array(sorted_absval(result)).astype(int)
+
+def sorted_absval(x):
+    return sorted(x, key=abs, reverse=True)
 
 def anomaly_free_set(l, k):
     
@@ -25,13 +29,41 @@ def anomaly_free_set(l, k):
         return linear_combination(vp, vm)
 
     else:
-        raise RuntimeError("The input is wrong! Fix it!")
+        raise RuntimeError("The input is wrong!")
 
+def number_generator(n):
 
+    upper = int(n/2)
 
-l = np.array([1, -1])
-k = np.array([2, 0])
+    if n % 2 == 0:
+    
+        m = int(n/2 - 1)
+        
+        l = np.random.randint(1, upper, m)*(-1)**np.random.randint(0, 2, m)
+        k = np.random.randint(1, upper, m)*(-1)**np.random.randint(0, 2, m)
+    
+    elif n % 2 != 0:
+    
+        m = int((n-3)/2 - 1)
+    
+        l = np.random.randint(1, upper, m)*(-1)**np.random.randint(0, 2, m)
+        k = np.random.randint(1, upper, m+1)*(-1)**np.random.randint(0, 2, m)
+    else:
+        raise RuntimeError("The input is wrong!")
+    
+    return l, k
+
+x_ = np.random.randint(1, 6, 6)*(-1)**np.random.randint(0, 2, 6)
+
+print(sorted_absval(x_))
+
+print(number_generator(6))
+
+l = np.array([1, 2])
+k = np.array([1, -2])
 
 z = anomaly_free_set(l, k)
 
 print(z, z.sum(), (z**3).sum())
+
+# funtion to sorted accoding to the absolute value
